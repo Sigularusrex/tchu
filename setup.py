bd26c6c8894c1a9477238b6e9c97e56e4fd4c230
David Sigley <davidsigley@davids-mbp.home>
2024-01-09 20:48:30 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='chu',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     install_requires=[
         'pika',  # Add other dependencies here
