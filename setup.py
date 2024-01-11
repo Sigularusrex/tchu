1f39171b39735df22ced0560795701149ea21b77
David Sigley <davidsigley@Davids-MacBook-Pro.local>
2024-01-11 15:38:38 +0100

    added code comments and bumped to version 1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="chu",
-    version="0.10",
+    version="1.0",
     packages=find_packages(),
     install_requires=[
         "pika",  # Add other dependencies here
